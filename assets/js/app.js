3f29351595a1c5faf650ccd3fa1b907355cb81c8
witek746653-maker <witek746653@gmail.com>
2025-12-15 18:32:58 +0300

    Update app.js

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,7 +1,7 @@
 if ("serviceWorker" in navigator) {
-  navigator.serviceWorker.register("../../sw.js");
+  navigator.serviceWorker.register("./sw.js");
 }
-fetch('../../components/contact-modal.html')
+fetch('./components/contact-modal.html')
   .then(res => res.text())
   .then(html => {
     document.getElementById('modal-root').innerHTML = html;
